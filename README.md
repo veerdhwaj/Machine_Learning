87bdf4b7e38e2c771903fc35e4e84a1ba94e8d8a
veerdhwaj <veerdhwaj@aol.com>
2018-05-28 22:39:55 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Machine_Learning
+# Twitter Sentiment Analysis
+
+This is the code for the Twitter Sentiment Analyzer. Instead of printing out each tweet,we are here saving each Tweet to a CSV file with an associated label. The label should be either 'Positive' or 'Negative'.
+
+
+##Dependencies
+
+tweepy (http://www.tweepy.org/)
+textblob (https://textblob.readthedocs.io/en/dev/)
+Install missing dependencies using pip
+
+##Usage
+
+Once you have your dependencies installed via pip, run the script in terminal via
+
+python demo.py
+
+The code uses the tweepy library to access the Twitter API and the TextBlob library to perform Sentiment Analysis on each Tweet. 
---
+++ tweet.py
@@ -0,0 +1,38 @@
+import tweepy
+from textblob import TextBlob
+import csv
+
+
+consumer_key='ldcdB1zPex9PvlJE3lZObn39s'
+consumer_secret='1zDYs2k0yLeC3fkaamFPU0D8AZeEmdTCi34k1rnZJTuOQkSiRO'
+access_token = '41557794-DZKSYfnj2u25auIF846oEkyfk26IOyGE67qBZlGLC'
+access_token_secret = 'aPwNkI5JxnbGOPEyF3Pnxe4AuQQmCsrq30xbuaVXp3yfx'
+
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+api=tweepy.API(auth)
+
+public_tweets = api.search('India')
+f = open('tweet.csv', 'w')
+with f:
+	writer = csv.DictWriter(f, fieldnames = ["Tweet", "Analysis 1"])
+	writer.writeheader()
+	writer = csv.writer(f)
+
+
+
+	for tweet in public_tweets:
+	    print(tweet.text)
+	    
+	    analysis = TextBlob(tweet.text)
+	    tweet_polarity = analysis.sentiment.polarity
+	    if tweet_polarity>0.0 and tweet_polarity<=1.0:
+	    	data = 'positive'
+	    else:
+	    	data = 'negative'
+
+	    inpt=[tweet.text,data]
+	    
+	    writer.writerow(inpt)
+f.close()	    
